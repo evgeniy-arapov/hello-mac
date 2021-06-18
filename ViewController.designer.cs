b2ff4dc31e662aaff466121f680b6965d7a97f36
Evgeniy Arapov <arapov.evgeniy@gmail.com>
2021-06-18 23:25:12 +0300

    first commit

---
+++ ViewController.designer.cs
@@ -0,0 +1,31 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Rider IDE
+//   to store outlets and actions made in Xcode.
+// If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using Foundation;
+using System.CodeDom.Compiler;
+
+namespace Hello_Mac
+{
+	[Register ("ViewController")]
+	partial class ViewController
+	{
+		[Outlet]
+		AppKit.NSTextField ClickedLabel { get; set; }
+
+		[Action ("ClickedButton:")]
+		partial void ClickedButton (Foundation.NSObject sender);
+
+		void ReleaseDesignerOutlets ()
+		{
+			if (ClickedLabel != null) {
+				ClickedLabel.Dispose ();
+				ClickedLabel = null;
+			}
+
+		}
+	}
+}
